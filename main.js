ec9444bac7676d0352e2adf5c9b8cb3fe21d6594
TeaChareqishvili <teachareqishvili89@gmail.com>
2022-10-23 02:47:17 +0400

    javascript

---
+++ main.js
@@ -0,0 +1,32 @@
+let myName = "Tea";
+let myLastName = "Chareqishvili";
+
+let myHobby = "coding";
+const myBirthYear = 1989;
+let currentYear = 2022;
+let city = "tbilisi";
+let month = "may";
+let date = 3;
+
+console.log( typeof myName , myLastName);
+console.log(typeof myHobby, myBirthYear);
+
+let myAge = currentYear - myBirthYear;
+
+console.log(myAge);
+
+const text1 = "Hello my  name is " + myName;
+
+const text2 = "I live in " + city;
+const text3 = "I was born in " + myBirthYear + " " + date + " of " + month;
+const text4 = "my hobby is " + myHobby + ".";
+
+
+
+
+
+console.log(text1, text2,text3, text4,);
+
+
+
+
